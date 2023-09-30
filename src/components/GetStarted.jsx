5909aa0a646df7c8793469682184e6a526baac44
Kevvan D. Young <101077732+kdy1994kko@users.noreply.github.com>
2023-09-30 08:42:58 -0600

    Add files via upload

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import styles from "../style";
+import { arrowUp } from "../assets";
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () => (
+  <a href="https://www.biblegateway.com/passage/?search=Psalm+23%3A4&version=TLV" target="_blank" rel="noreferrer">
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+            
+            <span className="text-gradient">Fear No Evil</span>
+          </p>
+          <img src={arrowUp} alt="arrow-up" className="w-[23px] h-[23px] object-contain" />
+        </div>
 
-export default GetStarted
+        <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
+          <span className="text-gradient">Psalm 23:4</span>
+        </p>
+        
+      </div>
+    </div>
+  </a>
+);
+
+export default GetStarted;
+
+
